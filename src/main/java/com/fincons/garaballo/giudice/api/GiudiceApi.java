41fa21ab5e8102d08e03353e1120320a7a5ed621
Alessio Lorenzo Trotta <alessio.trotta@finconsgroup.com>
2026-03-19 17:03:50 +0100

    aggiunta struttura chiamata rest

---
+++ src/main/java/com/fincons/garaballo/giudice/api/GiudiceApi.java
@@ -0,0 +1,9 @@
+package com.fincons.garaballo.giudice.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/default")
+public interface GiudiceApi {
+
+}
---
+++ src/main/java/com/fincons/garaballo/giudice/controller/GiudiceController.java
@@ -0,0 +1,6 @@
+package com.fincons.garaballo.giudice.controller;
+
+import com.fincons.garaballo.giudice.api.GiudiceApi;
+
+public class GiudiceController implements GiudiceApi {
+}
