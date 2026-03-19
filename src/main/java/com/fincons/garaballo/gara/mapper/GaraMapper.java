0799c6753ac9a022955dd08d4269b7f17ceca499
Sandro Romanazzi <sandro.romanazzi@finconsgroup.com>
2026-03-19 16:13:43 +0100

    primo

--- src/main/java/com/fincons/garaballo/gara/mapper/GaraMapper.java
+++ src/main/java/com/fincons/garaballo/gara/mapper/GaraMapper.java
@@ -1,5 +1,17 @@
 package com.fincons.garaballo.gara.mapper;
 
-//@Mapper
+import com.fincons.garaballo.gara.dto.GaraDto;
+import com.fincons.garaballo.gara.entity.Gara;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
 public interface GaraMapper {
+
+    GaraMapper INSTANCE = Mappers.getMapper(GaraMapper.class);
+
+    Gara toGara(GaraDto garaDto);
+
+    GaraDto toGaraDto(Gara gara);
 }
+
